368599ff90419791f3fdf71158f87fbd20b83745
Lee Barry <lee@Lees-Mac-mini.local>
2025-06-10 20:58:18 +0100

    chore(tests): remove unused React imports from test files to fix TS6133 build errors

--- eveonline-helper/src/components/__tests__/SkillPlanExportButton.test.tsx
+++ eveonline-helper/src/components/__tests__/SkillPlanExportButton.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { SkillPlanExportButton } from '../SkillPlanExportButton';
 import * as skillPlanUtils from '../../hooks/useSkillPlan';
